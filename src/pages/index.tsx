453cc4242ac88790133fb7b58eb5c084a8645b9b
yancarlodev <yancarlodc@gmail.com>
2023-01-19 11:12:43 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/index.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.boxContainer}>
+            <section className={styles.boxLeft}>
+                a
+            </section>
+            <section className={styles.boxRight}>
+                a
+            </section>
+        </div>
+    </div>
+  )
+}
